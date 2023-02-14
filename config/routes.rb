acec5c438ec28c03252bdcab237f9fa4ef87cd4a
geromon <123849679+geromon89@users.noreply.github.com>
2023-02-14 02:46:30 +0000

    [finish]5-9

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   root to:"homes#top"
 
+  resources :post_image, only: [:new,:index,:show]
+
+  get '/homes/about' => 'homes#about',as:'about'#pass:zunda4423
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
